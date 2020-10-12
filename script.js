3d97d246f44a1ae5d32fe9d67b7e457dd366f73c
Upshur06 <mupshur2005@gmail.com>
2020-10-12 14:05:44 -0400

    fetch URL

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /**
  * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
+ * DONE: Complete getWeatherData() to return json response Promise
  * TODO: Complete searchCity() to get user input and get data using getWeatherData()
  * TODO: Complete showWeatherData() to set the data in the the html file from response
  */
 
 // API_KEY for maps api
-let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+let API_KEY = "";
 
 /**
  * Retrieve weather data from openweathermap
@@ -17,10 +17,18 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
   //HINT: Use template literals to create a url with input and an API key
-
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
   //CODE GOES HERE
+  // console.log(FULL_URL);
+
+  const weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  });
 };
 
+console.log(getWeatherData("Detroit"));
+// getWeatherData("Boston");
 /**
  * Retrieve city input and get the weather data
  * HINT: Use the promise returned from getWeatherData()
