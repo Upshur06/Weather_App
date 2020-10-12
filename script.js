485026e32db9ad11c9a7bb093e2b2a3aad09b631
Upshur06 <mupshur2005@gmail.com>
2020-10-12 13:11:42 -0400

    template model

--- script.js
+++ script.js
@@ -6,27 +6,34 @@
  */
 
 // API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
 /**
  * Retrieve weather data from openweathermap
  * HINT: Use fetch()
  * HINT: URL should look like this:
  * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
  */
+getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  //HINT: Use template literals to create a url with input and an API key
 
-//HINT: Use template literals to create a url with input and an API key
-
-//CODE GOES HERE
+  //CODE GOES HERE
+};
 
 /**
  * Retrieve city input and get the weather data
  * HINT: Use the promise returned from getWeatherData()
  */
-
-// CODE GOES HERE
+searchCity = () => {
+  const city = document.getElementById("city-input").value;
+  // CODE GOES HERE
+};
 
 /**
  * Show the weather data in HTML
  * HINT: make sure to console log the weatherData to see how the data looks like
  */
-
-//CODE GOES HERE
+showWeatherData = (weatherData) => {
+  //CODE GOES HERE
+};
